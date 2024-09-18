0393d932b1e994440991af9983fa763335e919c5
demin1078 <dima.gpom@gmail.com>
2024-09-18 19:49:27 +0300

    changed to dJango

---
+++ car_evaluation/evaluate/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import TodoItem
+import random
+
+def evaluate_car(request):
+    car_characteristics = {
+        'Make': 'Toyota',
+        'Model': 'Camry',
+        'Year': '2020',
+        'Mileage': '15,000 miles',
+        'Color': 'White'
+    }
+    estimated_price = ''
+    if request.method == 'POST':
+        estimated_price = f"${random.randint(5000, 30000)}"
+    return render(request, 'evaluate/evaluate_car.html', {
+        'car_characteristics': car_characteristics,
+        'estimated_price': estimated_price
+    })
+
+def database(request):
+    return render(request, 'evaluate/database.html')
+
+def about(request):
+    return render(request, 'evaluate/about.html')
