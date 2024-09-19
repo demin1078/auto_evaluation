20a4ead5224b2ed62fd285b957ac9b90311875bd
demin1078 <dima.gpom@gmail.com>
2024-09-19 09:45:03 +0300

    add env variables

--- car_evaluation/car_evaluation/settings.py
+++ car_evaluation/car_evaluation/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uvhk91j@t(m7i$gj$qvo0uy42hcs7dx7ncsdatwq7ov7(0-_i0'
+# SECURITY WARNING: keep the secret key used in production secret! # django-insecure-uvhk91j@t(m7i$gj$qvo0uy42hcs7dx7ncsdatwq7ov7(0-_i0
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
