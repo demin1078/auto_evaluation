c0beb590c471c37c1e2da194f8ec9b0b77d3dc82
demin1078 <dima.gpom@gmail.com>
2024-10-13 22:04:40 +0300

    change model2

--- evaluate/models.py
+++ evaluate/models.py
@@ -11,14 +11,14 @@ class Cars(models.Model):
     title = models.CharField(max_length=200)
     car_name = models.CharField(max_length=200)
     photo_url  = models.CharField(max_length=550)
-    make = models.CharField(max_length=200)
-    model = models.CharField(max_length=200)
-    year = models.CharField(max_length=200)
-    color = models.CharField(max_length=200)
+    make = models.CharField(max_length=200, null=True)
+    model = models.CharField(max_length=200, null=True)
+    year = models.FloatField(max_length=200, null=True)
+    color = models.CharField(max_length=200, null=True)
 
-    short_description = models.CharField(max_length=200)
-    long_description = models.CharField(max_length=200)
+    short_description = models.CharField(max_length=200, null=True)
+    long_description = models.CharField(max_length=200, null=True)
 
-    mileage = models.FloatField()
-    price = models.FloatField()
+    mileage = models.FloatField( null=True)
+    price = models.FloatField( null=True)
 
