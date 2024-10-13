a4507adc0336d155ed5853d56e0e505379a2543b
demin1078 <dima.gpom@gmail.com>
2024-10-13 21:42:57 +0300

    change DB structure

--- evaluate/models.py
+++ evaluate/models.py
@@ -11,5 +11,14 @@ class Cars(models.Model):
     title = models.CharField(max_length=200)
     car_name = models.CharField(max_length=200)
     photo_url  = models.CharField(max_length=550)
+    make = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    year = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+
+    short_description = models.CharField(max_length=200)
+    long_description = models.CharField(max_length=200)
+
+    mileage = models.FloatField()
     price = models.FloatField()
 
